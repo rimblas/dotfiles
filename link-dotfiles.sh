e52e85eafa066a91adad1f6dcabd98f345254a9a
Dan Callahan <dan.callahan@gmail.com>
2010-05-03 16:44:25 -0500

    Add script to automagically link the dotfiles.

---
+++ link-dotfiles.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+OLDPWD=$PWD
+
+cd "`dirname $0`"
+
+# Ask user to remove any physical directories before linking
+FLAG=0
+for f in ./files/.[A-z]*; do
+    if [ -d "$HOME/`basename $f`" -a ! -L "$HOME/`basename $f`" ]; then
+        if [ $FLAG -eq 0 ]; then
+            echo "Please remove:"
+        fi
+        echo "$HOME/`basename $f`"
+        FLAG=1
+    fi
+done
+
+# Bali if any physical directories were present
+if [ $FLAG -ne 0 ]; then
+    echo "Then re-run this script to install symlinks."
+    exit 1
+fi
+
+# Link the dotfiles
+for f in ./files/.[A-z]*; do
+    # Overwrite symlinks without asking for confirmation
+    if [ -L "$HOME/`basename $f`" ]; then
+        LNOPTS="-vsfh"
+    else
+        LNOPTS="-vsih"
+    fi
+    ln $LNOPTS "$PWD/files/`basename $f`" "$HOME/`basename $f`"
+done
+
+cd "$OLDPWD"

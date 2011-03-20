1f9d55f2ca98f7845b0b8fbe4ed047fb8fe38e82
Dan Callahan <dan.callahan@gmail.com>
2011-03-20 15:21:10 -0400

    Reorganize zsh stuff similarly to oh-my-zsh

---
+++ files/.zsh/lib/aliases.zsh
@@ -0,0 +1,26 @@
+alias ls='ls -AFG'
+export LSCOLORS=ExGxFxdxCxDxDxxbaDacae
+
+export GREP_OPTIONS='--color=auto'
+export GREP_COLOR='1;32'
+
+# Use MacVim in the console if available
+if [[ -x /usr/local/bin/mvim ]]; then
+    alias vi="mvim -v"
+
+    alias ex="mvim -ve"
+    alias vim="mvim -v"
+    alias view="mvim -vR"
+    alias vimdiff="mvim -vd"
+    alias rvim="mvim -vZ"
+    alias rview="mvim -vRZ"
+
+    alias gex="mvim -ge"
+    alias gvim="mvim -g"
+    alias gview="mvim -gR"
+    alias gvimdiff="mvim -gd"
+    alias rgvim="mvim -gZ"
+    alias rgview="mvim -gRZ"
+fi
+
+export EDITOR=vim
---
+++ files/.zsh/lib/zshmisc.zsh
@@ -0,0 +1,16 @@
+setopt autocd
+
+setopt extendedglob
+
+setopt multios
+
+setopt correct
+
+setopt noclobber
+
+DIRSTACKSIZE=10
+setopt autopushd
+
+setopt long_list_jobs
+
+bindkey -v

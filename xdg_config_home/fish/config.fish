76a4ac02e977d29c13d982253a4108b470169475
Dan Callahan <dan.callahan@gmail.com>
2016-08-11 12:16:16 -0500

    Clean up fish config

--- xdg_config_home/fish/config.fish
+++ xdg_config_home/fish/config.fish
@@ -8,15 +8,10 @@ if command -v lesspipe.sh > /dev/null
 end
 
 # Paths
-begin
-    set -l EXISTING
-    for DIR in "$HOME/.cargo/bin" "$HOME/.gocode/bin"
-        if test -d $DIR
-            set EXISTING $EXISTING $DIR
-        end
+for DIR in "$HOME/.cargo/bin" "$HOME/.gocode/bin"
+    if test -d $DIR; and not contains $fish_user_paths $DIR
+        set fish_user_paths $fish_user_paths $DIR
     end
-
-    set fish_user_paths $EXISTING
 end
 
 # Golang
@@ -32,3 +27,5 @@ set theme_date_format "+%H:%M:%S"
 set theme_display_vi "yes"
 set theme_display_vi_hide_mode "insert"
 set theme_show_exit_status "yes"
+
+set -e fish_greeting

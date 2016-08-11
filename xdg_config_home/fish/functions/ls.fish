01f3eac83b1c92e26cd436a53440365e87b75c6b
Dan Callahan <dan.callahan@gmail.com>
2016-08-11 10:09:58 -0500

    Update Fish aliases to work on macOS and Linux

--- xdg_config_home/fish/functions/ls.fish
+++ xdg_config_home/fish/functions/ls.fish
@@ -1,7 +1,17 @@
 function ls --description 'List contents of directory'
-	set -l param --almost-all --color=auto
-	if isatty 1
-		set param $param --indicator-style=classify
+	set -l param
+
+	if test (uname -s) = 'Darwin' # macOS
+		set param -A -G
+		if isatty 1
+			set param $param -F
+		end
+	else # Linux
+		set param --almost-all --color=auto
+		if isatty 1
+			set param $param --indicator-style=classify
+		end
 	end
+
 	command ls $param $argv
 end
--- xdg_config_home/fish/functions/rm.fish
+++ xdg_config_home/fish/functions/rm.fish
@@ -1,3 +1,9 @@
 function rm
-	command rm -i --preserve-root $argv
+	set -l params -i
+
+	if test (uname -s) = 'Linux'
+		set params $params --preserve-root
+	end
+
+	command rm $params $argv
 end

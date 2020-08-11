d6866bc39ea9755ecd11df4d198d7a99dc0538c7
Jorge Rimblas <jrimblas@drwholdings.com>
2020-08-11 16:04:19 -0500

    New paths

--- xdg_config_home/fish/config.fish
+++ xdg_config_home/fish/config.fish
@@ -40,15 +40,16 @@ set -e fish_greeting
 
 # Global node global packages (for the user)
 # add_user_path ~/node/npm_packages/bin
-add_user_path ~/node/npm_packages/bin
+add_user_path ~/work/node_modules/bin
+
 
 # aliases for sql=sqlplus and sqlcl
-alias sql='rlwrap sqlplus'
+# alias sql='rlwrap sqlplus'
 alias sqlcl='~/oracle/sqlcl/bin/sql'
 
 # export variables for Oracle
 # set -x DYLD_LIBRARY_PATH ~/oracle/instantclient_11_2
-set -x DYLD_LIBRARY_PATH ~/oracle/product/instantclient/12.2.0.1.0
+set -x DYLD_LIBRARY_PATH ~/oracle/products/instantclient_19_3
 set -x TNS_ADMIN ~/oracle/network/admin
 set -x SQLPATH ~/oracle/sql
 
